884b54b7e1c37be5ef35997b67b37d8554c4fde1
Sawai Hiroshi <info@info-town.jp>
2012-12-29 11:15:25 +0900

    Add documentation(yuidoc)

---
+++ out/api.js
@@ -0,0 +1,11 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "bundary",
+        "filter",
+        "processing"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
