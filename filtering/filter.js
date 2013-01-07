09ffa47a54c3d675cb2fd225f91f9ff5e800d3fe
Hiroshi Sawai <info@info-town.jp>
2013-01-07 11:32:25 +0900

    Add filter

--- filtering/filter.js
+++ filtering/filter.js
@@ -25,13 +25,36 @@ jQuery(function($) {
          * @type {Object}
          */
         var operator = {
-            smooth: [1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9],
-            mean: [1/16, 2/16, 1/16, 2/16, 4/16, 2/16, 1/16, 2/16, 1/16],
-            sharpen: [0, -1, 0, -1, 5, -1, 0, -1, 0],
-            differentialH: [0, 0, 0, 0, -1, 1, 0, 0, 0], // 横方向一次微分フィルター
-            differentialV: [0, 1, 0, 0, -1, 0, 0, 0, 0], // 横方向一次微分フィルター
-            prewitt: [-1, 0, 1, -1, 0, 1, -1, 0, 1]         // Prewittフィルター
+            smooth: [1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9],           // 平滑化
+            mean: [1/16, 2/16, 1/16, 2/16, 4/16, 2/16, 1/16, 2/16, 1/16],    // 平滑化(加重平均)
+            sharpen: [0, -1, 0, -1, 5, -1, 0, -1, 0],                        // 先鋭化
+            sharpen2: [-1, -1, -1, -1, 9, -1, -1, -1, -1],                   // 先鋭化2
+            differentialH: [0, 0, 0, 0, -1, 1, 0, 0, 0],                     // 横方向一次微分フィルター
+            differentialV: [0, 1, 0, 0, -1, 0, 0, 0, 0],                     // 横方向一次微分フィルター
+            prewitt: [-1, 0, 1, -1, 0, 1, -1, 0, 1],                         // Prewittフィルター
+            sobel: [-1, 0, 1, -2, 0, 2, -1, 0, 1],                            // Sobelフィルター
+            emboss: [1, 0, 0, 0, -1, 0, 0, 0, 0]                             // Embossフィルター
         };
+
+        /**
+         * 反転
+         * @method @reverse
+         * @private
+         * @param {Number} k 注目ピクセルのRed値に対応するImageData.dataのインデックス<br>
+         *     green k + 1<br>
+         *     blue  k + 2<br>
+         *     alpha k + 3
+         * @param {ImageObject} imageData
+         * @return {Object} rgbaの値を格納したオブジェクト
+         */
+        function reverse(k, imageData) {
+            var rgba = {};
+            rgba.r = 255 - imageData.data[k];
+            rgba.g = 255 - imageData.data[k + 1];
+            rgba.b = 255 - imageData.data[k + 2];
+            rgba.a = imageData.data[k + 3];
+            return rgba;
+        }
 
         /**
          * 2値画像フィルター
@@ -80,6 +103,72 @@ jQuery(function($) {
         }
 
         /**
+         * 赤を増やす
+         *
+         * @method red
+         * @private
+         * @param {Number} k 注目ピクセルのRed値に対応するImageData.dataのインデックス<br>
+         *     green k + 1<br>
+         *     blue  k + 2<br>
+         *     alpha k + 3
+         * @param {ImageObject} imageData
+         * @return {Object} rgbaの値を格納したオブジェクト
+         */
+        function red(k, imageData) {
+            var rgba = {};
+            rgba.r = imageData.data[k];
+            rgba.r = (rgba.r < 250) ? rgba.r + 5 : 255;
+            rgba.g = imageData.data[k + 1];
+            rgba.b = imageData.data[k + 2];
+            rgba.a = imageData.data[k + 3];
+            return rgba;
+        }
+
+        /**
+         * グリーンを増やす
+         *
+         * @method green
+         * @private
+         * @param {Number} k 注目ピクセルのRed値に対応するImageData.dataのインデックス<br>
+         *     green k + 1<br>
+         *     blue  k + 2<br>
+         *     alpha k + 3
+         * @param {ImageObject} imageData
+         * @return {Object} rgbaの値を格納したオブジェクト
+         */
+        function green(k, imageData) {
+            var rgba = {};
+            rgba.r = imageData.data[k];
+            rgba.g = imageData.data[k + 1];
+            rgba.g = (rgba.g < 250) ? rgba.g + 5 : 255;
+            rgba.b = imageData.data[k + 2];
+            rgba.a = imageData.data[k + 3];
+            return rgba;
+        }
+
+        /**
+         * 青を増やす
+         *
+         * @method blue
+         * @private
+         * @param {Number} k 注目ピクセルのRed値に対応するImageData.dataのインデックス<br>
+         *     green k + 1<br>
+         *     blue  k + 2<br>
+         *     alpha k + 3
+         * @param {ImageObject} imageData
+         * @return {Object} rgbaの値を格納したオブジェクト
+         */
+        function blue(k, imageData) {
+            var rgba = {};
+            rgba.r = imageData.data[k];
+            rgba.g = imageData.data[k + 1];
+            rgba.b = imageData.data[k + 2];
+            rgba.b = (rgba.b < 250) ? rgba.b + 5 : 255;
+            rgba.a = imageData.data[k + 3];
+            return rgba;
+        }
+
+        /**
          * 空間フィルター
          *
          * @method spatial
@@ -92,6 +181,9 @@ jQuery(function($) {
          * @return {Object}
          */
         function spatial(k, imageData, name) {
+            if (operator[name] instanceof Array === false) {
+                throw new Exception('フィルターがありません。');
+            }
             var rgba = {};
             rgba.r = rgba.g = rgba.b = 0;
             var i, j, n, count = 0, index = {};
@@ -125,25 +217,23 @@ jQuery(function($) {
             if ('grayscale' === name) {
                 return grayscale(k, imageData);
             }
-            if ('smooth' === name) {
-                return spatial(k, imageData, name);
+            if ('reverse' === name) {
+                return reverse(k, imageData);
             }
-            if ('mean' === name) {
-                // 加重平均
-                return spatial(k, imageData, name);
-
+            if ('red' === name) {
+                return red(k, imageData);
             }
-            if ('sharpen' === name) {
-                return spatial(k, imageData, name);
+            if ('green' === name) {
+                return green(k, imageData);
             }
-            if ('differentialH' === name) {
-                return spatial(k, imageData, name);
+            if ('blue' === name) {
+                return blue(k, imageData);
             }
-            if ('differentialV' === name) {
+            // 空間フィルター
+            try {
                 return spatial(k, imageData, name);
-            }
-            if ('prewitt' === name) {
-                return spatial(k, imageData, name);
+            } catch (e) {
+                alert('フィルターがありません。');
             }
         }
 
